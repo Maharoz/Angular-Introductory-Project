038f2b084d3b72e7fc5796eac86a60b368b7ce8b
Maharoz <mahruj66@gmail.com>
2018-11-10 20:28:33 +0600

    adding directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -3,12 +3,22 @@ import { getOrCreateInjectable } from '@angular/core/src/render3/di';
 
 @Component({
     selector : 'courses' ,
-    template : '<h2>{{ "Title: " + title }}</h2>'
+    template : `
+                <h2>{{ title }}</h2>
+                <ul>
+                <li *ngFor="let course of courses">
+                {{ course }}
+                </li>
+                </ul>
+    `
 })
+//here *ngFor is the directives and let course of courses iterating the courses
 export class CoursesComponent{
-    title= "List of courses"
+    title= "List of courses";
+    courses = ["course1","course2","course3"];
 
     getTitle(){
         return this.title;
     }
 }
+//ng g c course
