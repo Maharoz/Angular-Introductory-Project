f97a7495515d2d7c352d6c33076a11be5d9fa917
Maharoz <mahruj66@gmail.com>
2018-11-13 23:50:35 +0600

    switch case demonstrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { FavouriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- courses=[1,2];
+viewMode = 'map';
 }
