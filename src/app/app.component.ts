437d6107a88b342df8c2f02985a48df6bb07d678
Maharoz <mahruj66@gmail.com>
2018-11-14 20:37:55 +0600

    track by implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,17 @@ import { FavouriteChangedEventArgs } from './favorite/favorite.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-courses=[
+courses;
+
+loadCourses(){
+this.courses=[
   {id:1 , name:'course 1'},
   {id:2 , name:'course 2'},
   {id:3 , name:'course 3'}
 ];
-
-onAdd(){
-  this.courses.push({id:4 , name : 'course 4'});
 }
 
-onRemove(course){
-let index = this.courses.indexOf(course);
-this.courses.splice(index,1);
+trackCourse(index,course){
+  return course? course.id : undefined;
 }
 }
