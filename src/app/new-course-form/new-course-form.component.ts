060a1c0437d0e1486e895fd2691a3d42bb66d133
Maharoz <mahruj66@gmail.com>
2018-11-18 21:16:42 +0600

    use of form builder class demonstrated

--- src/app/new-course-form/new-course-form.component.ts
+++ src/app/new-course-form/new-course-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormGroup, FormControl } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-course-form',
@@ -9,25 +9,19 @@ import { FormArray, FormGroup, FormControl } from '@angular/forms';
 export class NewCourseFormComponent implements OnInit {
 
 
-  form = new FormGroup({
-    topics : new FormArray([])
-  });
+  form ;
 
 
-  addTopic(topic : HTMLInputElement){
-    this.topics.push(new FormControl(topic.value));
-    topic.value = '';
-  }
-
-  removeTopic(topic :FormControl){
-  let index = this.topics.controls.indexOf(topic);
-  this.topics.removeAt(index);
-  }
-
-  get topics(){
-    return this.form.get('topics') as FormArray;
-  }
-  constructor() { }
+  constructor(fb : FormBuilder) {
+   this.form = fb.group({
+      name:['',Validators.required],
+      contact : fb.group({
+        email:[],
+        phone:[]
+      }),
+      topics: fb.array([])
+    });
+   }
 
   ngOnInit() {
   }
