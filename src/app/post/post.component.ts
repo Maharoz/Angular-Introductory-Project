a880869ce594f9988f06b7ea4c08a36682f3800e
Maharoz <mahruj66@gmail.com>
2018-11-19 19:48:37 +0600

    ng on init implemented

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -12,40 +12,16 @@ export class PostComponent implements OnInit {
   posts : any[];
   private url='https://jsonplaceholder.typicode.com/posts';
   constructor(private http: Http) {
-    http.get(this.url)
+   
+   }
+
+   
+  ngOnInit() {
+    this.http.get(this.url)
     .subscribe(response =>{
       //console.log(response.json());
       this.posts = response.json();
     });
-   }
-
-   createPost(input : HTMLInputElement){
-     let post = { title : input.value };
-     input.value ='';
-    this.http.post(this.url,JSON.stringify(post))
-    .subscribe(response => {
-      post['id'] = response.json().id;
-      this.posts.splice(0,0, post);
-      
-    });
-   }
-
-   UpdatePost(post){
-     this.http.patch(this.url + '/' + post.id , JSON.stringify({ isRead : true}))
-     .subscribe(response =>{
-      console.log(response.json());
-     })
-   }
-
-   deletePost(post){
-     this.http.delete(this.url + '/' + post.id)
-     .subscribe(response =>{
-       let index = this.posts.indexOf(post);
-       this.posts.splice(index,1);
-     })
-   }
-
-  ngOnInit() {
   }
 
 }
